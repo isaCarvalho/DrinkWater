ff36f79dc1b68dcf5fbb9c2580807a6b5ac4b503
isaCarvalho <isabelacarvalhocf@hotmail.com>
2020-07-12 20:12:45 -0300

    History

--- app/src/main/java/com/example/drinkwater/HistoryActivity.kt
+++ app/src/main/java/com/example/drinkwater/HistoryActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
@@ -21,34 +22,75 @@ class HistoryActivity : AppCompatActivity() {
 
         val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
 
+        // list of rows
+        val rows = ArrayList<TableRow>()
+        var textsContent: ArrayList<String>
+
+        // creating the table header
+        textsContent = ArrayList()
+        textsContent.add(resources.getString(R.string.total_water))
+        textsContent.add(resources.getString(R.string.percent))
+        textsContent.add(resources.getString(R.string.quantity))
+        textsContent.add(resources.getString(R.string.date))
+
+        rows.add(makeRow(textsContent))
+
+
+        // creating the table data
         val list = WaterHelper.getAll(this)
         list?.forEach { item ->
-            val row =  TableRow(this)
-            val lp = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
-            row.layoutParams = lp
 
-            val totalWaterTxt = TextView(this)
-            totalWaterTxt.text = "${item.getTotalWater()} L"
+            textsContent = ArrayList()
+            textsContent.add(item.getTotalWater().toString())
+            textsContent.add(item.getPercent().toString())
+            textsContent.add(item.getQtWater().toString())
+            textsContent.add(item.getDate())
 
-            val totalWaterMLTxt = TextView(this)
-            totalWaterMLTxt.text = "${item.getTotalWaterML()} ml"
+            rows.add(this.makeRow(textsContent, list.indexOf(item) + 1))
+        }
 
-            val percentTxt = TextView(this)
-            percentTxt.text = "${item.getPercent()} %"
+        // appending the row
+        rows.forEach {
+            tableLayout.addView(it)
+        }
+    }
 
-            val qtWaterTxt = TextView(this)
-            qtWaterTxt.text = "${item.getQtWater()} ml"
+    private fun makeRow(textsContent : ArrayList<String>, index : Int = 0) : TableRow
+    {
+        val lp = TableRow.LayoutParams(
+            TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
 
-            val dateTxt = TextView(this)
-            dateTxt.text = item.getDate()
+        val row =  TableRow(this).apply {
+            layoutParams = lp
 
-            row.addView(totalWaterTxt)
-            row.addView(totalWaterMLTxt)
-            row.addView(percentTxt)
-            row.addView(qtWaterTxt)
-            row.addView(dateTxt)
+            if ((index % 2) == 0)
+                setBackgroundColor(resources.getColor(R.color.colorTextLight))
+            else
+                setBackgroundColor(resources.getColor(R.color.colorText))
+        }
 
-            tableLayout.addView(row, list.indexOf(item))
+        // creating the text views
+        val textViews = ArrayList<TextView>()
+
+        textsContent.forEach { textContent ->
+            textViews.add(makeTextView(textContent))
+        }
+
+        // appending the text views in the row
+        textViews.forEach { textView -> row.addView(textView) }
+
+        return row
+    }
+
+    private fun makeTextView(contentText : String) : TextView
+    {
+        return TextView(this).apply {
+            text = contentText
+            setTextColor(resources.getColor(R.color.colorBackground))
+            gravity = Gravity.CENTER_HORIZONTAL
+            setPadding(4,4,4,4)
         }
     }
 
